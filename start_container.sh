1a3b488ab567664d78c0b7cc5c044fe75746b154
nikhitha98M <mnikhitha9795@gmail.com>
2024-12-13 23:52:53 +0530

    Create start_container.sh

---
+++ start_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+# Pull the Docker image from Docker Hub
+docker pull nikhitha529/my-repository
+# Run the Docker image as a container
+docker run -d -p 5000:5000 nikhitha529/my-repository
+
+
+
+
