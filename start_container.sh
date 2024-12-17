04cee441bb8e9956d5e0e60029e362bef50c1476
nikhitha98M <mnikhitha9795@gmail.com>
2024-12-18 00:26:55 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -3,7 +3,7 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull nikhitha529/my-repository
 # Run the Docker image as a container
-docker run -d -p 5000:5000 nikhitha529/my-repository
+docker run -d -p 6000:6000 nikhitha529/my-repository
 
 
 
